84b99e67dd45cc7a4dd703768b15d3971d53cb0e
Yoshikawa Ayano <yoshikawa@codepro2019MBA01.local>
2020-12-13 19:23:25 +0900

    add: 空のプロジェクトと.gitignoreを作成

---
+++ Nikolaos_SwiftUI/Nikolaos_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nikolaos_SwiftUI
+//
+//  Created by Yoshikawa Ayano on 2020/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nikolaos_SwiftUI/Nikolaos_SwiftUI/Nikolaos_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nikolaos_SwiftUIApp.swift
+//  Nikolaos_SwiftUI
+//
+//  Created by Yoshikawa Ayano on 2020/12/13.
+//
+
+import SwiftUI
+
+@main
+struct Nikolaos_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
