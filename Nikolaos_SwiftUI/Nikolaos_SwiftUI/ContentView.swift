fe3c27f58a29df1f434762e67c25656f5981a730
Yoshikawa Ayano <yoshikawa@codepro2019MBA01.local>
2020-12-15 21:03:22 +0900

    fix: mergeのためにconflictを解消

--- Nikolaos_SwiftUI/Nikolaos_SwiftUI/ContentView.swift
+++ Nikolaos_SwiftUI/Nikolaos_SwiftUI/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            OfferingListView()
+                .tabItem {
+                    Text("募集一覧")
+                }
+                .tag(0)
+            OfferView()
+                .tabItem {
+                    Text("募集する")
+                }
+                .tag(1)
+            CameraView()
+                .tabItem {
+                    Text("QR読取")
+                }
+                .tag(2)
+            ReservingListView()
+                .tabItem {
+                    Text("受取予定")
+                }
+                .tag(3)
+        }
     }
 }
 
---
+++ Nikolaos_SwiftUI/Nikolaos_SwiftUI/OfferingList/OfferingListView.swift
@@ -0,0 +1,20 @@
+//
+//  OfferingListView.swift
+//  Nikolaos_SwiftUI
+//
+//  Created by Yoshikawa Ayano on 2020/12/15.
+//
+
+import SwiftUI
+
+struct OfferingListView: View {
+    var body: some View {
+        Text("募集一覧")
+    }
+}
+
+struct OfferingListView_Previews: PreviewProvider {
+    static var previews: some View {
+        OfferingListView()
+    }
+}
