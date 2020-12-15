b558b11360e1698866d9c911f50b643d3c7eab49
enPiT2016MBP-09 <codepro@enPiT2016MBP-09.local>
2020-12-15 12:14:38 +0900

    add; 募集一覧画面のセル一個分作成

---
+++ Nikolaos_SwiftUI/Nikolaos_SwiftUI/OfferingList/OfferingRowView.swift
@@ -0,0 +1,21 @@
+//
+//  OfferingRowView.swift
+//  Nikolaos_SwiftUI
+//
+//  Created by enPiT2016MBP-09 on 2020/12/15.
+//
+
+import SwiftUI
+
+struct OfferingRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OfferingRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        OfferingRowView()
+            .previewLayout(.fixed(width: 400, height: 100))
+    }
+}
