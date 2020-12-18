3189e9859b41478e96d66d6c466ea3373fb26acf
Yoshikawa Ayano <yoshikawa@codepro2019MBA01.local>
2020-12-19 08:46:15 +0900

    mod: 表示が収まるように修正

--- Nikolaos_SwiftUI/Nikolaos_SwiftUI/OfferingList/OfferingRowView.swift
+++ Nikolaos_SwiftUI/Nikolaos_SwiftUI/OfferingList/OfferingRowView.swift
@@ -9,26 +9,28 @@ import SwiftUI
 
 struct OfferingRowView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 0.0) {
-            Text("30cm定規 ×1")
-                .multilineTextAlignment(.leading)
-            Text("カッター ×1")
-                .multilineTextAlignment(.leading)
-            Text("ガムテープ ×1")
-            Text("体育館横　1017XXX")
-                .font(.footnote)
-                .padding(.leading, 220.0)
-                .frame(width: 400.0)
-                .multilineTextAlignment(.trailing)
+        HStack{
+            VStack(alignment: .leading, spacing: 0.0) {
+                Text("30cm定規 ×1")
+                    .multilineTextAlignment(.leading)
+                Text("カッター ×1")
+                    .multilineTextAlignment(.leading)
+                Text("ガムテープ ×1")
+            }
+            Spacer()
+            VStack() {
+                Spacer()
+                Text("体育館横　1017XXX")
+                    .font(.footnote)
+                    .multilineTextAlignment(.trailing)
+            }
         }
         .padding()
-        .frame(width: 400.0, height: 100.0, alignment: .topLeading)
     }
 }
 
 struct OfferingRowView_Previews: PreviewProvider {
     static var previews: some View {
         OfferingRowView()
-            .previewLayout(.fixed(width: 400, height: 100))
     }
 }
